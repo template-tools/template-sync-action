ee0b94abb7c213768ceab264da3897b94c8e8193
Markus Felten <Markus.Felten@gmx.de>
2021-01-08 00:01:10 +0100

    fix: use import

--- index.mjs
+++ index.mjs
@@ -1,15 +1,11 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setFailed } from "@actions/core";
+import github from "@actions/github";
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const template = getInput("template");
+  console.log(`Hello ${template}!`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
