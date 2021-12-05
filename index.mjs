bfb789f1921b8d807a8843e5409814bd3b91ce1f
Markus Felten <Markus.Felten@gmx.de>
2021-12-05 21:05:59 +0100

    feat: do the real work

--- index.mjs
+++ index.mjs
@@ -1,11 +1,34 @@
 import { getInput, setFailed } from "@actions/core";
 import github from "@actions/github";
+import { Context } from "@template-tools/template-sync";
+import GithubProvider from "github-repository-provider";
+
+
+async function execute(payload)
+{
+  const options = {};
+  const provider = GithubProvider.initialize(undefined, process.env);
+
+  console.log(provider);
+  const context = await Context.from(
+    provider,
+    payload.repository.html_url,
+    options
+  );
+
+  for await (const pr of context.execute()) {
+    console.log(pr);
+  }
+}
 
 try {
   const template = getInput("template");
-  console.log(`Hello ${template}!`);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(`Hello ${template}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2);
+  //console.log(`The event payload: ${payload}`);
+
+  execute(github.context.payload);
+
 } catch (error) {
   setFailed(error.message);
 }
